57436ada0211bdf4f06a3b805e6557ea9fee4b38
Patrick489 <78151815+Patrick489@users.noreply.github.com>
2021-04-22 11:50:55 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -232,11 +232,11 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
-        f"\n♻️`𝙋𝙖𝙩𝙧𝙞𝙘𝙠𝘽𝙤𝙩 𝙄𝙨 𝘼𝙘𝙩𝙞𝙫𝙚𝙙`🇮🇩\n"
+        f"\n ♻️𝙋𝙖𝙩𝙧𝙞𝙘𝙠𝘽𝙤𝙩 𝙄𝙨 𝘼𝙘𝙩𝙞𝙫𝙚𝙙🇮🇩\n"
         f"╭━━━━━━━━━━━━━━━━━━━╮\n"
         f"┣[•👤 USER     : {DEFAULTUSER}\n"
         f"┣[•👁‍🗨 Username : @{user.username}\n"
-        "`┣▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n"
+        f"┣▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n"
         f"┣[•⚙️ Telethon : v {version.__version__} 🔥\n"
         f"┣[•🐍 Python   : v {python_version()} 🔥\n"
         f"┣[•💻 Base on  : {UPSTREAM_REPO_BRANCH}🔥\n"
@@ -244,7 +244,7 @@ async def amireallyalive(alive):
         f"┣[•🗃 Modules  : {len(modules)} Loaded 🔥\n"
         f"┣[•🕒 Uptime   : {uptime} 🔥\n"
         f"╰━━━━━━━━━━━━━━━━━━━╯\n"
-        f" •MOD BY : `{DEFAULTUSER}`")
+        f" •MOD BY : {DEFAULTUSER}")
     f" •REPO  : [𝙋𝙖𝙩𝙧𝙞𝙘𝙠𝘽𝙤𝙩](https://github.com/Patrick489/PatrickBot)"
     if ALIVE_LOGO:
         try:
