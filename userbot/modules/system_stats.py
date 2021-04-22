d88eedf99b3322892c5a4254366fd6534efef3ae
Patrick489 <78151815+Patrick489@users.noreply.github.com>
2021-04-22 11:40:23 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -232,17 +232,17 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
-        f"\n      ♻️`𝙋𝙖𝙩𝙧𝙞𝙘𝙠𝘽𝙤𝙩 𝙄𝙨 𝘼𝙘𝙩𝙞𝙫𝙚𝙙`🇮🇩\n"
+        f"\n♻️`𝙋𝙖𝙩𝙧𝙞𝙘𝙠𝘽𝙤𝙩 𝙄𝙨 𝘼𝙘𝙩𝙞𝙫𝙚𝙙`🇮🇩\n"
         f"╭━━━━━━━━━━━━━━━━━━━━━╮\n"
         f"┣[•👤 USER     :`{DEFAULTUSER}`\n"
-        f"┣[ 👁‍🗨 Username :`@{user.username}`\n"
+        f"┣[•👁‍🗨 Username : @{user.username}\n"
         "`┣▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱`\n"
-        f"┣[•⚙️ Telethon :`v {version.__version__}🔥`\n"
-        f"┣[•🐍 Python   :`v {python_version()}🔥`\n"
+        f"┣[•⚙️ Telethon :`v {version.__version__} 🔥`\n"
+        f"┣[•🐍 Python   :`v {python_version()} 🔥`\n"
         f"┣[•💻 Base on  :`{UPSTREAM_REPO_BRANCH}🔥`\n"
-        f"┣[•🛠 Version  :`{BOT_VER}🔥`\n"
-        f"┣[•🗃 Modules  :`{len(modules)} Loaded🔥`\n"
-        f"┣[•🕒 Uptime   :`{uptime}🔥`\n"
+        f"┣[•🛠 Version  :`{BOT_VER} 🔥`\n"
+        f"┣[•🗃 Modules  :`{len(modules)} Loaded 🔥`\n"
+        f"┣[•🕒 Uptime   :`{uptime} 🔥`\n"
         f"╰━━━━━━━━━━━━━━━━━━━━━╯\n"
         f" •MOD BY : `{DEFAULTUSER}`")
     f" •REPO  : [𝙋𝙖𝙩𝙧𝙞𝙘𝙠𝘽𝙤𝙩](https://github.com/Patrick489/PatrickBot)"
